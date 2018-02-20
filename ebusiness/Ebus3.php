f2befa6676146547b0ffa6da207de40cb873d4cc
Amy Dolan <117407532@umail.ucc.ie>
2018-02-20 13:14:43 +0000

    pushing to github

--- ebusiness/Ebus3.php
+++ ebusiness/Ebus3.php
@@ -3,7 +3,53 @@ session_start();
 ?>
 <!DOCTYPE html>
 <html>
-    <head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<head>
+<style>
+body {margin:0;}
+
+ul {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    overflow: hidden;
+    background-color: #333;
+    position: fixed;
+    top: 0;
+    width: 100%;
+}
+
+li {
+    float: left;
+}
+
+li a {
+    display: block;
+    color: white;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+}
+
+li a:hover:not(.active) {
+    background-color: #111;
+}
+
+.active {
+    background-color: #FFD1DC;
+}
+</style>
+</head>
+<body>
+
+<ul>
+  <li><a class="active" href="../homepage.html">Home</a></li>
+  <li><a href="cv/cv_page1.html">CV</a></li>
+  <li><a href="interests/sports.html">Interests</a></a></li>
+  <li><a href="ebusiness/Ebus1.php">E-Business</a></li>
+</ul>
+
+<div style="padding:20px;margin-top:30px;background-color:#DFECFF;height:1500px;">
         <title>RECEIPT</title>
     </head>
     <body>
@@ -13,5 +59,6 @@ session_start();
         //Echo session variables that were set on previus page
         echo "Total is " . $_SESSION["total"] . ".";
         ?>
+        </div>
     </body>
 </html>
--- ebusiness/ebus2_validator.js
+++ ebusiness/ebus2_validator.js
@@ -2,6 +2,24 @@
 
 function validateDetails(){
     
+    //validating user name details
+    
+      var name;
+    
+    name = document.getElementById("user_name").value;
+    
+    if (name==""){
+        alert("Please enter your name");
+        }
+    else if (String(pin).length<5){
+        alert("Please make sure you enter your full name");
+        }
+    else{
+        enablebtnPurchase();
+        }
+    
+    //validating user pin details
+    
     var pin;
     
     pin = document.getElementById("user_pin").value;
