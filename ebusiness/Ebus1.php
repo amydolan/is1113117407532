a0fc28df02139a4c8d417a195259895f171852c5
Amy Dolan <117407532@umail.ucc.ie>
2018-02-23 12:39:27 +0000

    pushing to github

--- ebusiness/Ebus1.php
+++ ebusiness/Ebus1.php
@@ -2,40 +2,7 @@
 <html>
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <head>
-<style>
-body {margin:0;}
-
-ul {
-    list-style-type: none;
-    margin: 0;
-    padding: 0;
-    overflow: hidden;
-    background-color: #333;
-    position: fixed;
-    top: 0;
-    width: 100%;
-}
-
-li {
-    float: left;
-}
-
-li a {
-    display: block;
-    color: white;
-    text-align: center;
-    padding: 14px 16px;
-    text-decoration: none;
-}
-
-li a:hover:not(.active) {
-    background-color: #111;
-}
-
-.active {
-    background-color: #FFD1DC;
-}
-</style>
+<link rel="stylesheet" href="../mystylesheet.css" type="text/css" />
 </head>
 <body>
 
